e76154635407ac70ff9ddd05106fab90f7f4c74f
Vadim <chuvak117@gmail.com>
2018-06-04 12:50:30 +0300

    Update

--- code/getProxies_test.go
+++ code/getProxies_test.go
@@ -1,10 +1,13 @@
 package code
 
 import (
+	"os"
 	"testing"
 )
 
 func TestGetProxies(t *testing.T) {
+	os.Setenv("URL", "http://tproxyt.tk/json")
+	os.Setenv("FI", "password")
 	if err := getProxies(); err != nil {
 		t.Fail()
 	}
